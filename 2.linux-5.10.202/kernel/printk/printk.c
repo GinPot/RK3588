ff358610b9f12bb6028a45bf00b0f511783a758a
Ping <929391874@qq.com>
2024-04-10 22:58:31 +0800

    [tty] 删除多余的tty设备，防止因其他tty设备禁止掉earlycon.

--- 2.linux-5.10.202/kernel/printk/printk.c
+++ 2.linux-5.10.202/kernel/printk/printk.c
@@ -2765,6 +2765,16 @@ void register_console(struct console *newcon)
 	unsigned long flags;
 	struct console *bcon = NULL;
 	int err;
+	static int earlycon_flag;
+
+	if(earlycon_flag){
+		if(strncmp(newcon->name, "ttyS", 4) != 0){
+			//pr_info("GinPot: %s,%s\n", __func__, newcon->name);
+			//dump_stack();
+			return;
+		}
+	}
+	earlycon_flag = 1;
 
 	for_each_console(bcon) {
 		if (WARN(bcon == newcon, "console '%s%d' already registered\n",
